b3866a4b0dafaf5154778e5f7e9f39efbe31612b
Mohan Radhakrishnan <rmohankumar@ness.com>
2013-01-17 12:49:11 -0500

    Project skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dayo::Application.config.secret_token = 'e0503469547a8d0ae5e27b7c1540416862d8eb1c972eb0033fbde6f7c5019e0708dddc4311b07e66ac24fc78a2dcec4f313f6be2365f955a2ec60f61b18a37ac'
