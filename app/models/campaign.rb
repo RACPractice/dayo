e41df49df67f5c63ee4493ff837e2f346813d756
Iulian Dogariu <code@iuliandogariu.com>
2013-01-28 11:49:57 +0200

    Fixed bug in campaign UI after introducing a many-to-many relationship between Campaign and Route. Added ability to specify one route for a campaign.

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,5 +1,7 @@
  class Campaign < ActiveRecord::Base
   attr_accessible :from_email, :from_name, :html_url, :name, :reply_to, :subject, :text_url, :user_id, :base_airlines, :comparative_airlines, :advance_days, :length_of_stay, :score
+  attr_accessible :routes_text
+
 
   #ASSOCIATIONS
   belongs_to :user
@@ -7,4 +9,28 @@
   has_and_belongs_to_many :routes
   has_and_belongs_to_many :schedules
   has_and_belongs_to_many :lists
+
+  # Produces a text description of all the routes
+  def routes_text
+    routes.map { |r| r.text_descriptor }.join("; ")
+  end
+
+  # Accepts a string of routes in text form,
+  # and populates the routes association
+  def routes_text=(v)
+    routes.clear
+    v.split(/;\s*/).each do |r|
+      origin, destination = r.split /\s*-\s*/
+      next unless origin.present? && destination.present?
+      logger.info "Asked to set origin #{origin} and destination #{destination}"
+      route_obj = Route.find_by_origin_and_destination(origin, destination)
+      if route_obj.present?
+        routes << route_obj
+        logger.info "Found Route #{route_obj}"
+      else
+        logger.warn "Could not find a Route"
+      end
+    end
+  end
+
 end
--- app/models/route.rb
+++ app/models/route.rb
@@ -1,5 +1,16 @@
 class Route < ActiveRecord::Base
   attr_accessible :origin, :destination
   validates :origin, :destination, :length => {:is => 3} # IATA airport codes
-  validates :origin, :destination, :presence => true 
+  validates :origin, :destination, :presence => true
+
+  def self.options_for_routes_select
+    all.map do |route|
+      [route.text_descriptor, route.text_descriptor]
+    end
+  end
+
+  def text_descriptor
+    "#{origin} - #{destination}"
+  end
+
 end
