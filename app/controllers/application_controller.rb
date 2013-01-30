fe35b7f1c571786b5276aabd60b35c869f0950b4
Silviu Rosu <silviu.rosu@gmail.com>
2013-01-30 12:06:08 +0200

    add http authentication for heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  http_basic_authenticate_with :name => "dayo", :password => "secret" unless Rails.env.development?
+
 end
