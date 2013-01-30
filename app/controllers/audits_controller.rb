b4a2ac305ee739c02dca178d6ebf1137616f7060
Iulian Dogariu <code@iuliandogariu.com>
2013-01-30 18:27:36 +0200

    Work around a stupid default_scope in Audited

--- app/controllers/audits_controller.rb
+++ app/controllers/audits_controller.rb
@@ -1,7 +1,14 @@
 class AuditsController < ApplicationController
 
+  #class  ::Audited::Adapters::ActiveRecord::Audit
+  #  default_scope(order('created_at DESC'))
+  #end
+
   def index
-    @audits = Audited::Adapters::ActiveRecord::Audit.order('created_at DESC').paginate(:page => params[:page], :per_page => 15)
+    Audited::Adapters::ActiveRecord::Audit.send(:with_exclusive_scope) do
+      # Work around a stupid default_scope in Audited
+      @audits = Audited::Adapters::ActiveRecord::Audit.order('created_at DESC').paginate(:page => params[:page], :per_page => 15)
+    end
 
     respond_to do |format|
       format.html # index.html.erb
