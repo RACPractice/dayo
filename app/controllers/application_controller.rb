f6fe20371f1392068ce979c9ebb72520bc822561
Iulian Dogariu <code@iuliandogariu.com>
2013-01-30 14:02:11 +0200

    Don't ask for HTTP auth in test mode.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  http_basic_authenticate_with :name => "dayo", :password => "secret" unless Rails.env.development?
+  http_basic_authenticate_with :name => "dayo", :password => "secret" if Rails.env.production?
 
 end
